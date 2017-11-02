0339e307fc5d65ab1f806cfcc2b0ac9f9f6f56ee
Philip Niedertscheider <phil@techprimate.com>
2017-11-02 02:00:54 +0100

    Added more tests

--- Source/Graphics/PDFGraphics.swift
+++ Source/Graphics/PDFGraphics.swift
@@ -7,70 +7,75 @@
 //
 
 class PDFGraphics {
-    
+
+    // MARK: - Lines
+
     static func drawLine(start: CGPoint, end: CGPoint, style: PDFLineStyle) {
-        if style.type == .none {
-            return
+        if let path = createLinePath(start: start, end: end, style: style) {
+            style.color.setStroke()
+
+            path.stroke()
         }
-        
-        let path = UIBezierPath()
+    }
+
+    static func createLinePath(start: CGPoint, end: CGPoint, style: PDFLineStyle) -> UIBezierPath? {
+        if style.type == .none {
+            return nil
+        }
+
+        var path = UIBezierPath()
         path.move(to: start)
         path.addLine(to: end)
-        
+
+        let dashes = createDashes(style: style, path: &path)
+        path.setLineDash(dashes, count: dashes.count, phase: 0.0)
         path.lineWidth = CGFloat(style.width)
-        
-        var dashes: [CGFloat] = []
-        
-        switch style.type {
-        case .dashed:
-            dashes = [path.lineWidth * 3, path.lineWidth * 3]
-            path.lineCapStyle = .butt
-        case .dotted:
-            dashes = [0, path.lineWidth * 2]
-            path.lineCapStyle = .round
-        default:
-            break
-        }
-        
-        path.setLineDash(dashes, count: dashes.count, phase: 0.0)
-        
-        // Set color to line
-        style.color.setStroke()
-        
-        path.stroke()
+
+        return path
     }
-    
-    static func drawRect(rect: CGRect, outline: PDFLineStyle, fill: UIColor = UIColor.clear) {
-        let path = UIBezierPath(rect: rect)
-        
-        path.lineWidth = CGFloat(outline.width)
-        
-        var dashes: [CGFloat] = []
-        
-        switch outline.type {
-        case .dashed:
-            dashes = [path.lineWidth * 3, path.lineWidth * 3]
-            path.lineCapStyle = .butt
-        case .dotted:
-            dashes = [0, path.lineWidth * 2]
-            path.lineCapStyle = .round
-        default:
-            break
-        }
-        
-        path.setLineDash(dashes, count: dashes.count, phase: 0.0)
-        
-        // Set color to line
+
+    static func drawRect(rect: CGRect, outline: PDFLineStyle, fill: UIColor = .clear) {
+        let path = createRectPath(rect: rect, outline: outline)
+
         outline.color.setStroke()
         fill.setFill()
-        
+
         path.fill()
         path.stroke()
     }
-    
+
+    static func createRectPath(rect: CGRect, outline: PDFLineStyle) -> UIBezierPath {
+        var path = UIBezierPath(rect: rect)
+
+        let dashes = createDashes(style: outline, path: &path)
+        path.setLineDash(dashes, count: dashes.count, phase: 0.0)
+        path.lineWidth = CGFloat(outline.width)
+
+        return path
+    }
+
+    static func createDashes(style: PDFLineStyle, path: inout UIBezierPath) -> [CGFloat] {
+        var dashes: [CGFloat] = []
+
+        switch style.type {
+        case .dashed:
+            dashes = [style.width * 3, style.width * 3]
+            path.lineCapStyle = .butt
+        case .dotted:
+            dashes = [0, style.width * 2]
+            path.lineCapStyle = .round
+        default:
+            break
+        }
+
+        return dashes
+    }
+
+    // MARK: - Image
+
     static func resizeAndCompressImage(image: UIImage, frame: CGRect, quality: CGFloat) -> UIImage {
         // resize
-        let resizeFactor = (3 * quality > 1) ? 3 * quality : 1
+        let resizeFactor = (3 * quality > 1) ? 1 : 3 * quality
         let resizeImageSize = CGSize(width: frame.size.width * resizeFactor, height: frame.size.height * resizeFactor)
         
         UIGraphicsBeginImageContext(resizeImageSize)
