039bf9d46afe56c4ff5ae8725d35e94975fd84a0
Philip Niedertscheider <phil@techprimate.com>
2017-11-17 10:42:19 +0100

    Added method for comparing two colors with a delta Added spec for PDFIndentationObject Equatable Added method to extract pixel from image and spec

--- Example/Tests/Graphics/UIImage+Pixel_Spec.swift
+++ Example/Tests/Graphics/UIImage+Pixel_Spec.swift
@@ -13,42 +13,43 @@ import Nimble
 class UIImage_Pixel_Spec: QuickSpec {
 
     override func spec() {
-        describe("UIImage Pixel extension") {
+        describe("UIImage") {
 
-            it("can get pixel color") {
-                let size = CGSize(width: 1, height: 1)
+            context("Pixel") {
+                it("can get pixel color") {
+                    UIGraphicsBeginImageContext(CGSize(width: 2, height: 2))
 
-                UIGraphicsBeginImageContext(CGSize(width: 2 * size.width, height: 2 * size.height))
+                    var rect = CGRect(x: 0, y: 0, width: 1, height: 1)
+                    var path = UIBezierPath(rect: rect)
+                    UIColor.blue.setFill()
+                    path.fill()
 
-                var rect = CGRect(origin: .zero, size: size)
-                var path = UIBezierPath(rect: rect)
-                UIColor.blue.setFill()
-                path.fill()
+                    rect = CGRect(x: 1, y: 0, width: 1, height: 1)
+                    path = UIBezierPath(rect: rect)
+                    UIColor.green.setFill()
+                    path.fill()
 
-                rect = CGRect(x: size.width, y: 0, width: size.width, height: size.height)
-                path = UIBezierPath(rect: rect)
-                UIColor.green.setFill()
-                path.fill()
+                    rect = CGRect(x: 0, y: 1, width: 1, height: 1)
+                    path = UIBezierPath(rect: rect)
+                    UIColor.red.setFill()
+                    path.fill()
 
-                rect = CGRect(x: 0, y: size.height, width: size.width, height: size.height)
-                path = UIBezierPath(rect: rect)
-                UIColor.red.setFill()
-                path.fill()
+                    rect = CGRect(x: 1, y: 1, width: 1, height: 1)
+                    path = UIBezierPath(rect: rect)
+                    UIColor.orange.setFill()
+                    path.fill()
 
-                rect = CGRect(x: size.width, y: size.height, width: size.width, height: size.height)
-                path = UIBezierPath(rect: rect)
-                UIColor.orange.setFill()
-                path.fill()
+                    let image: UIImage! = UIGraphicsGetImageFromCurrentImageContext()!
+                    UIGraphicsEndImageContext()
 
-                let _ = UIGraphicsGetImageFromCurrentImageContext()!
-                UIGraphicsEndImageContext()
+                    // Position is upside down
 
-                // Position is upside down
-
-//                expect(image.getPixelColor(at: CGPoint(x: 0         , y: 0          ))) == UIColor.red
-//                expect(image.getPixelColor(at: CGPoint(x: size.width, y: 0          ))) == UIColor.green
-//                expect(image.getPixelColor(at: CGPoint(x: 0         , y: size.height))) == UIColor.red
-//                expect(image.getPixelColor(at: CGPoint(x: size.width, y: size.height))) == UIColor.orange
+                    expect(image.size) == CGSize(width: 2, height: 2)
+                    expect(image.pixelColor(at: CGPoint(x: 0, y: 0))) == UIColor.blue
+                    expect(image.pixelColor(at: CGPoint(x: 1, y: 0))) == UIColor.green
+                    expect(image.pixelColor(at: CGPoint(x: 0, y: 1))) == UIColor.red
+                    expect(image.pixelColor(at: CGPoint(x: 1, y: 1)).isClose(to: UIColor.orange, decimals: 2)).to(beTrue())
+                }
             }
         }
     }
--- Source/Graphics/UIImage+Pixel.swift
+++ Source/Graphics/UIImage+Pixel.swift
@@ -5,20 +5,80 @@
 //  Created by Philip Niedertscheider on 05/11/2017.
 //
 
+// swiftlint:disable identifier_name
+
 extension UIImage {
 
-    func getPixelColor(at location: CGPoint) -> UIColor {
-        let width = self.size.width
-        let pixelData = self.cgImage!.dataProvider!.data
-        let data = CFDataGetBytePtr(pixelData)!
+    func pixelColor(at location: CGPoint) -> UIColor {
+        return PixelExtractor(img: self.cgImage!).colorAt(x: Int(location.x), y: Int(location.y))
+    }
+}
 
-        let pixelIndex: Int = Int(4 * (location.y * width + location.x))
+/**
+ https://stackoverflow.com/questions/35029672/getting-pixel-color-from-an-image-using-cgpoint
+ */
+class PixelExtractor: NSObject {
 
-        let r = CGFloat(data[pixelIndex])     / CGFloat(255.0)
-        let g = CGFloat(data[pixelIndex + 1]) / CGFloat(255.0)
-        let b = CGFloat(data[pixelIndex + 2]) / CGFloat(255.0)
-        let a = CGFloat(data[pixelIndex + 3]) / CGFloat(255.0)
-        
-        return UIColor(red: r, green: g, blue: b, alpha: a)
+    let image: CGImage
+    let context: CGContext?
+
+    var width: Int {
+        return image.width
+    }
+
+    var height: Int {
+        return image.height
+    }
+
+    init(img: CGImage) {
+        image = img
+        context = PixelExtractor.createBitmapContext(cgImage: img)
+    }
+
+    class func createBitmapContext(cgImage: CGImage) -> CGContext {
+
+        // Get image width, height
+        let pixelsWide = cgImage.width
+        let pixelsHigh = cgImage.height
+
+        let bitmapBytesPerRow = pixelsWide * 4
+        let bitmapByteCount = bitmapBytesPerRow * Int(pixelsHigh)
+
+        // Use the generic RGB color space.
+        let colorSpace = CGColorSpaceCreateDeviceRGB()
+
+        // Allocate memory for image data. This is the destination in memory
+        // where any drawing to the bitmap context will be rendered.
+        let bitmapData = malloc(bitmapByteCount)
+        let bitmapInfo = CGBitmapInfo(rawValue: CGImageAlphaInfo.premultipliedFirst.rawValue)
+        let size = CGSize(width: pixelsWide, height: pixelsHigh)
+        UIGraphicsBeginImageContextWithOptions(size, false, 0.0)
+        // create bitmap
+        let context = CGContext(data: bitmapData, width: pixelsWide, height: pixelsHigh, bitsPerComponent: 8,
+                                bytesPerRow: bitmapBytesPerRow, space: colorSpace, bitmapInfo: bitmapInfo.rawValue)
+
+        // draw the image onto the context
+        let rect = CGRect(x: 0, y: 0, width: pixelsWide, height: pixelsHigh)
+        context?.draw(cgImage, in: rect)
+
+        return context!
+    }
+
+    func colorAt(x: Int, y: Int)->UIColor {
+        assert(0<=x && x<width)
+        assert(0<=y && y<height)
+
+        let data = context!.data!
+
+        let offset = 4 * (y * width + x)
+
+        let alpha = CGFloat(data.load(fromByteOffset: offset, as: UInt8.self)) / 255.0
+        let red = CGFloat(data.load(fromByteOffset: offset+1, as: UInt8.self)) / 255.0
+        let green = CGFloat(data.load(fromByteOffset: offset+2, as: UInt8.self)) / 255.0
+        let blue = CGFloat(data.load(fromByteOffset: offset+3, as: UInt8.self)) / 255.0
+
+        let color = UIColor(red: red, green: green, blue: blue, alpha: alpha)
+
+        return color
     }
 }
