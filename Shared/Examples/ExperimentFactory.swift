06fc53ae2ac65a376daf252b0e0c7d8f17f1da3f
Philip Niedertscheider <dev@philpri.me>
2021-03-31 11:55:37 +0200

    Fixed available height calculation in PDFCalculations

--- Shared/Examples/ExperimentFactory.swift
+++ Shared/Examples/ExperimentFactory.swift
@@ -14,12 +14,37 @@ class ExperimentFactory: ExampleFactory {
     func generateDocument() -> [PDFDocument] {
         let document = PDFDocument(format: .a4)
 
-        document.pagination = .init(container: .footerRight)
-        
-        let externalDocument = PDFExternalDocument(url: Bundle.main.url(forResource: "sample-large", withExtension: "pdf")!)
-        document.add(externalDocument: externalDocument)
+        let items = (0..<40).map { $0.description }
 
-        document.add(text: "END")
+        // Simple bullet point list
+        let featureList = PDFList(indentations: [
+            (pre: 10.0, past: 20.0),
+            (pre: 20.0, past: 20.0),
+            (pre: 40.0, past: 20.0)
+        ])
+
+        // By adding the item first to a list item with the dot symbol, all of them will inherit it
+        featureList
+            .addItem(PDFListItem(symbol: .dot)
+                        .addItems(items.map({ item in
+                            PDFListItem(content: item)
+                        })))
+        document.add(list: featureList)
+
+        document.add(space: 20)
+
+        // Numbered list with unusual indentation
+        let weirdIndentationList = PDFList(indentations: [
+            (pre: 10.0, past: 20.0),
+            (pre: 40.0, past: 30.0),
+            (pre: 20.0, past: 50.0)
+        ])
+
+        weirdIndentationList
+            .addItems(items.enumerated().map({ arg in
+                PDFListItem(symbol: .numbered(value: "\(arg.offset + 1)"), content: arg.element)
+            }))
+        document.add(list: weirdIndentationList)
 
         return [document]
     }
