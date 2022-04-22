eb1754b7ffc0f37aacdd2216fe11812b8c1ea193
Patrick <patrickbdev@users.noreply.github.com>
2022-04-22 17:28:25 +0400

    Add name property on PDFPageFormat

---
+++ Source/API/Page Format/PDFPageFormat+NameConstants.swift
@@ -0,0 +1,153 @@
+#if os(iOS)
+import UIKit
+#elseif os(macOS)
+import AppKit
+#endif
+
+/**
+ This extension includes all name constants defined by their corresponding standard.
+ Source for names: https://www.papersizes.org
+ */
+public extension PDFPageFormat {
+    
+    /**
+     Returns the defined US names if this format is a US format.
+     If it is not a US format, it will check other constants for correct name
+     */
+    var usName: String {
+        switch self {
+        case .usHalfLetter:
+            return "US Half Letter"
+        case .usLetter:
+            return "US Letter"
+        case .usLegal:
+            return "US Legal"
+        case .usJuniorLegal:
+            return "US Junior Legal"
+        case .usLedger:
+            return "US Ledger"
+        default:
+            return name
+        }
+    }
+    
+    /**
+     Returns the defined ANSI name if this format is a ANSI format.
+     If it is not a ANSI format, it will check other constants for correct name
+     */
+    var ansiName: String {
+        switch self {
+        case .ansiA:
+            return "ANSI A"
+        case .ansiB:
+            return "ANSI B"
+        case .ansiC:
+            return "ANSI C"
+        case .ansiD:
+            return "ANSI D"
+        case .ansiE:
+            return "ANSI E"
+        default:
+            return name
+        }
+    }
+    
+    /**
+     Returns the defined A-Series name if this format is a A-Series format.
+     If it is not a A-Series format, it will check other constants for correct name
+     */
+    var aName: String {
+        switch self {
+        case .a0:
+            return "A0"
+        case .a1:
+            return "A1"
+        case .a2:
+            return "A2"
+        case .a3:
+            return "A3"
+        case .a4:
+            return "A4"
+        case .a5:
+            return "A5"
+        case .a6:
+            return "A6"
+        case .a7:
+            return "A7"
+        case .a8:
+            return "A8"
+        case .a9:
+            return "A9"
+        case .a10:
+            return "A10"
+        default:
+            return name
+        }
+    }
+    
+    /**
+     Returns the defined B-Series name if this format is a B-Series format.
+     If it is not a B-Series format, it will check other constants for correct name
+     */
+    var bName: String {
+        switch self {
+        case .b0:
+            return "B0"
+        case .b1:
+            return "B1"
+        case .b2:
+            return "B2"
+        case .b3:
+            return "B3"
+        case .b4:
+            return "B4"
+        case .b5:
+            return "B5"
+        case .b6:
+            return "B6"
+        case .b7:
+            return "B7"
+        case .b8:
+            return "B8"
+        case .b9:
+            return "B9"
+        case .b10:
+            return "B10"
+        default:
+            return name
+        }
+    }
+    
+    /**
+     Returns the defined C-Series name if this format is a C-Series format.
+     If it is not a C-Series format, it will check other constants for correct name
+     */
+    var cName: String {
+        switch self {
+        case .c0:
+            return "C0"
+        case .c1:
+            return "C1"
+        case .c2:
+            return "C2"
+        case .c3:
+            return "C3"
+        case .c4:
+            return "C4"
+        case .c5:
+            return "C5"
+        case .c6:
+            return "C6"
+        case .c7:
+            return "C7"
+        case .c8:
+            return "C8"
+        case .c9:
+            return "C9"
+        case .c10:
+            return "C10"
+        default:
+            return name
+        }
+    }
+}
---
+++ Tests/TPPDFTests/Page Format/PDFPageFormat+NameConstants_Spec.swift
@@ -0,0 +1,203 @@
+import CoreGraphics
+import Quick
+import Nimble
+@testable import TPPDF
+
+class PDFPageFormat_NameConstants_Spec: QuickSpec {
+
+    override func spec() {
+        describe("PDFPagename") {
+
+            context("NameConstants") {
+
+                context("US Series") {
+
+                    it("has a US Half Letter name") {
+                        expect(PDFPageFormat.usHalfLetter.name) == "US Half Letter"
+                    }
+
+                    it("has a US Letter name") {
+                        expect(PDFPageFormat.usLetter.name) == "US Letter"
+                    }
+
+                    it("has a US Legal name") {
+                        expect(PDFPageFormat.usLegal.name) == "US Legal"
+                    }
+
+                    it("has a US Junior Legal name") {
+                        expect(PDFPageFormat.usJuniorLegal.name) == "US Junior Legal"
+                    }
+
+                    it("has a US Ledger name") {
+                        expect(PDFPageFormat.usLedger.name) == "US Ledger"
+                    }
+                }
+
+                context("ANSI Series") {
+
+                    it("has a ANSI A name") {
+                        expect(PDFPageFormat.ansiA.name) == "ANSI A"
+                    }
+
+                    it("has a ANSI B name") {
+                        expect(PDFPageFormat.ansiB.name) == "ANSI B"
+                    }
+
+                    it("has a ANSI C name") {
+                        expect(PDFPageFormat.ansiC.name) == "ANSI C"
+                    }
+
+                    it("has a ANSI D name") {
+                        expect(PDFPageFormat.ansiD.name) == "ANSI D"
+                    }
+
+                    it("has a ANSI E name") {
+                        expect(PDFPageFormat.ansiE.name) == "ANSI E"
+                    }
+                }
+
+                context("A Series") {
+
+                    it("has a A0 name") {
+                        expect(PDFPageFormat.a0.name) == "A0"
+                    }
+
+                    it("has a A1 name") {
+                        expect(PDFPageFormat.a1.name) == "A1"
+                    }
+
+                    it("has a A2 name") {
+                        expect(PDFPageFormat.a2.name) == "A2"
+                    }
+
+                    it("has a A3 name") {
+                        expect(PDFPageFormat.a3.name) == "A3"
+                    }
+
+                    it("has a A4 name") {
+                        expect(PDFPageFormat.a4.name) == "A4"
+                    }
+
+                    it("has a A5 name") {
+                        expect(PDFPageFormat.a5.name) == "A5"
+                    }
+
+                    it("has a A6 name") {
+                        expect(PDFPageFormat.a6.name) == "A6"
+                    }
+
+                    it("has a A7 name") {
+                        expect(PDFPageFormat.a7.name) == "A7"
+                    }
+
+                    it("has a A8 name") {
+                        expect(PDFPageFormat.a8.name) == "A8"
+                    }
+
+                    it("has a A9 name") {
+                        expect(PDFPageFormat.a9.name) == "A9"
+                    }
+
+                    it("has a A10 name") {
+                        expect(PDFPageFormat.a10.name) == "A10"
+                    }
+                }
+
+                context("B Series") {
+
+                    it("has a B0 name") {
+                        expect(PDFPageFormat.b0.name) == "B0"
+                    }
+
+                    it("has a B1 name") {
+                        expect(PDFPageFormat.b1.name) == "B1"
+                    }
+
+                    it("has a B2 name") {
+                        expect(PDFPageFormat.b2.name) == "B2"
+                    }
+
+                    it("has a B3 name") {
+                        expect(PDFPageFormat.b3.name) == "B3"
+                    }
+
+                    it("has a B4 name") {
+                        expect(PDFPageFormat.b4.name) == "B4"
+                    }
+
+                    it("has a B5 name") {
+                        expect(PDFPageFormat.b5.name) == "B5"
+                    }
+
+                    it("has a B6 name") {
+                        expect(PDFPageFormat.b6.name) == "B6"
+                    }
+
+                    it("has a B7 name") {
+                        expect(PDFPageFormat.b7.name) == "B7"
+                    }
+
+                    it("has a B8 name") {
+                        expect(PDFPageFormat.b8.name) == "B8"
+                    }
+
+                    it("has a B9 name") {
+                        expect(PDFPageFormat.b9.name) == "B9"
+                    }
+
+                    it("has a B10 name") {
+                        expect(PDFPageFormat.b10.name) == "B10"
+                    }
+                }
+
+                context("C Series") {
+
+                    it("has a C0 name") {
+                        expect(PDFPageFormat.c0.name) == "C0"
+                    }
+
+                    it("has a C1 name") {
+                        expect(PDFPageFormat.c1.name) == "C1"
+                    }
+
+                    it("has a C2 name") {
+                        expect(PDFPageFormat.c2.name) == "C2"
+                    }
+
+                    it("has a C3 name") {
+                        expect(PDFPageFormat.c3.name) == "C3"
+                    }
+
+                    it("has a C4 name") {
+                        expect(PDFPageFormat.c4.name) == "C4"
+                    }
+
+                    it("has a C5 name") {
+                        expect(PDFPageFormat.c5.name) == "C5"
+                    }
+
+                    it("has a C6 name") {
+                        expect(PDFPageFormat.c6.name) == "C6"
+                    }
+
+                    it("has a C7 name") {
+                        expect(PDFPageFormat.c7.name) == "C7"
+                    }
+
+                    it("has a C8 name") {
+                        expect(PDFPageFormat.c8.name) == "C8"
+                    }
+
+                    it("has a C9 name") {
+                        expect(PDFPageFormat.c9.name) == "C9"
+                    }
+
+                    it("has a C10 name") {
+                        expect(PDFPageFormat.c10.name) == "C10"
+                    }
+                }
+            }
+        }
+    }
+
+}
