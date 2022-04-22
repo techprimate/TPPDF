d44ef294fb93779400c232d3d339830581d7fb2a
Patrick <patrickbdev@users.noreply.github.com>
2022-04-22 17:28:25 +0400

    Add name property on PDFPageFormat

--- Source/API/Page Format/PDFPageFormat.swift
+++ Source/API/Page Format/PDFPageFormat.swift
@@ -107,4 +107,22 @@ public enum PDFPageFormat: String, CaseIterable {
     public var landscapeSize: CGSize {
         CGSize(width: size.height, height: size.width)
     }
+    
+    /**
+     Name defined in constants
+     */
+    public var name: String {
+        switch self {
+        case .usHalfLetter, .usLetter, .usLegal, .usJuniorLegal, .usLedger:
+            return usName
+        case .ansiA, .ansiB, .ansiC, .ansiD, .ansiE:
+            return ansiName
+        case .a0, .a1, .a2, .a3, .a4, .a5, .a6, .a7, .a8, .a9, .a10:
+            return aName
+        case .b0, .b1, .b2, .b3, .b4, .b5, .b6, .b7, .b8, .b9, .b10:
+            return bName
+        case .c0, .c1, .c2, .c3, .c4, .c5, .c6, .c7, .c8, .c9, .c10:
+            return cName
+        }
+    }
 }
