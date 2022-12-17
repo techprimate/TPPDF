9a2415cc9350c1a74f7083670bc4e502e07f2a9e
Philip Niedertscheider <dev@philpri.me>
2022-12-17 12:42:04 +0100

    fix: fixed unnecessary equality methods

--- Source/API/Text/PDFText.swift
+++ Source/API/Text/PDFText.swift
@@ -8,27 +8,8 @@
 /// Protocol all text objects should implement
 public class PDFText: PDFDocumentObject, CustomStringConvertible {
 
-    var copy: PDFText {
+    public var copy: PDFText {
         fatalError()
-    }
-
-    /**
-     TODO: Documentation
-     */
-    public static func != (lhs: PDFText, rhs: PDFText) -> Bool {
-        !(lhs == rhs)
-    }
-
-    /**
-     TODO: Documentation
-     */
-    public static func == (lhs: PDFText, rhs: PDFText) -> Bool {
-        if let attributedLhs = lhs as? PDFAttributedText, let attributedRhs = rhs as? PDFAttributedText {
-            return attributedLhs == attributedRhs
-        } else if let simpleLhs = lhs as? PDFSimpleText, let simpleRhs = rhs as? PDFSimpleText {
-            return simpleLhs == simpleRhs
-        }
-        return false
     }
 }
 
